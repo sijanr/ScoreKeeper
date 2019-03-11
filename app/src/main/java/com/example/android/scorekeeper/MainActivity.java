dc20a1bebfbad5bac275bd64a4442f460e8ddc58
sijanr <sijan1.rijal@gmail.com>
2019-03-10 21:53:29 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.android.scorekeeper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Spinner spinner1 = findViewById(R.id.spinner_1);
+        final Spinner spinner2 = findViewById(R.id.spinner_2);
+
+        String[] colorOption = {"Black", "Green", "Blue", "Red"};
+
+        ArrayAdapter<String> newAdapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, colorOption);
+
+        newAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner1.setAdapter(newAdapter);
+        spinner2.setAdapter(newAdapter);
+
+        Button beginButton = findViewById(R.id.begin_button);
+
+        final Intent startGame = new Intent(MainActivity.this,game_layout.class);
+
+//      get the player 1 color
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        startGame.putExtra("COLOR_PLAYER1", android.R.color.black);
+                        break;
+
+                    case 1:
+                        startGame.putExtra("COLOR_PLAYER1", android.R.color.holo_green_dark);
+                        break;
+
+                    case 2:
+                        startGame.putExtra("COLOR_PLAYER1", android.R.color.holo_blue_dark);
+                        break;
+
+                    case 3:
+                        startGame.putExtra("COLOR_PLAYER1", android.R.color.holo_red_dark);
+                        break;
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+//      get the player 2 color
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                switch(i){
+                    case 0:
+                        startGame.putExtra("COLOR_PLAYER2", android.R.color.black);
+                        break;
+
+                    case 1:
+                        startGame.putExtra("COLOR_PLAYER2", android.R.color.holo_green_dark);
+                        break;
+
+                    case 2:
+                        startGame.putExtra("COLOR_PLAYER2", android.R.color.holo_blue_dark);
+                        break;
+
+                    case 3:
+                        startGame.putExtra("COLOR_PLAYER2", android.R.color.holo_red_dark);
+                        break;
+
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+//      pass the data from the first activity to the next activity
+        beginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+//                get the first player name
+                EditText firstPlayerName = (EditText) findViewById(R.id.firstPlayer);
+                if(firstPlayerName.getText().toString().equals("")){
+                    startGame.putExtra("FIRST_PLAYER","PLAYER 1");
+                }
+                else {
+                    startGame.putExtra("FIRST_PLAYER", firstPlayerName.getText().toString());
+                }
+
+
+//                get the second player name
+                EditText secondPlayerName= (EditText) findViewById(R.id.secondPlayer);
+                if(secondPlayerName.getText().toString().equals("")){
+                    startGame.putExtra("SECOND_PLAYER","PLAYER 2");
+                }
+                else{
+                    startGame.putExtra("SECOND_PLAYER",secondPlayerName.getText().toString());
+                }
+
+
+
+                startActivity(startGame);
+
+        }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/scorekeeper/game_layout.java
@@ -0,0 +1,104 @@
+package com.example.android.scorekeeper;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class game_layout extends AppCompatActivity {
+
+    private int firstPlayerScore = 0;
+    private int secondPlayerScore = 0;
+    private int countTime = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_layout);
+
+        final TextView gameOver = (TextView) findViewById(R.id.game_over);
+        gameOver.setVisibility(View.INVISIBLE);
+
+        final TextView winningPlayer = (TextView) findViewById(R.id.player_win);
+
+//      get the player names from the previous activity
+        Intent startGame = getIntent();
+        final TextView firstPlayerName = (TextView) findViewById(R.id.firstPlayerName);
+        firstPlayerName.setText(startGame.getStringExtra("FIRST_PLAYER"));
+        final TextView secondPlayerName = (TextView) findViewById(R.id.secondPlayerName);
+        secondPlayerName.setText(startGame.getStringExtra("SECOND_PLAYER"));
+
+//       get the players selected color and apply it to their names
+        firstPlayerName.setTextColor(ContextCompat.getColor(this, startGame.getIntExtra("COLOR_PLAYER1",0)));
+        secondPlayerName.setTextColor(ContextCompat.getColor(this,startGame.getIntExtra("COLOR_PLAYER2",0)));
+
+        final TextView firstPlayerResult = (TextView) findViewById(R.id.firstPlayerScore);
+        final TextView secondPlayerResult = (TextView) findViewById(R.id.secondPlayerScore);
+
+//        apply the selected colors to their respective score
+        firstPlayerResult.setTextColor(ContextCompat.getColor(this,startGame.getIntExtra("COLOR_PLAYER1",0)));
+        secondPlayerResult.setTextColor(ContextCompat.getColor(this,startGame.getIntExtra("COLOR_PLAYER2",0)));
+
+
+//        increase score for Player 1 when clicked the 'TAP' button
+        final Button firstPlayerButton = (Button) findViewById(R.id.firstPlayer_hit);
+        firstPlayerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                firstPlayerScore++;
+                firstPlayerResult.setText(Integer.toString(firstPlayerScore));
+            }
+        });
+
+
+//        increase score for Player 2 when clicked the 'TAP' button
+        final Button secondPlayerButton = (Button) findViewById(R.id.secondPlayer_hit);
+        secondPlayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                secondPlayerScore++;
+                secondPlayerResult.setText(Integer.toString(secondPlayerScore));
+            }
+        });
+
+        final TextView countDown = (TextView) findViewById(R.id.countDown_text_view);
+
+
+//        start the count down and display the game result once it finishes
+        new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long l) {
+                countTime++;
+                if(countTime==10){
+                    countDown.setText("00:"+Integer.toString(countTime));
+                }
+                else{
+                    countDown.setText("00:0"+Integer.toString(countTime));
+                }
+            }
+
+
+//          display the game score once its over
+            @Override
+            public void onFinish() {
+                firstPlayerButton.setEnabled(false);
+                secondPlayerButton.setEnabled(false);
+                gameOver.setVisibility(View.VISIBLE);
+                if(firstPlayerScore>secondPlayerScore){
+                    winningPlayer.setText(firstPlayerName.getText().toString()+" won the game");
+                }
+                else if(firstPlayerScore==secondPlayerScore){
+                    winningPlayer.setText("tie");
+                }
+                else{
+                    winningPlayer.setText(secondPlayerName.getText().toString()+" won the game");
+                }
+            }
+        }.start();
+    }
+}
